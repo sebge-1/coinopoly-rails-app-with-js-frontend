c2089cc521b84164d5f4eeef1a9f4861167cab48
Sebastian <sebgertz@gmail.com>
2018-08-08 13:59:28 -0400

    login and signup controller actions added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/signin'
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to root_path
+      end
+    end
+
+
+  def show
+    @user = User.find(params[:id])
+    if logged_in?
+      render 'show'
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :admin)
+  end
+
 end
