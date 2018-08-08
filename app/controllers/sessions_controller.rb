bf92f8ebad86ef5bedfef7c6c01ddab24a105dea
Sebastian <sebgertz@gmail.com>
2018-08-08 14:02:51 -0400

    added users_helper and user#show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
-def create
+  def create
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to user_path(@user)
-      else
-        redirect_to root_path
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to root_path
     end
+  end
 
 
   def show
