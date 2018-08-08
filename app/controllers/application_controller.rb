5e3802cc82246c1c93ca63d8dced30c9972e3c37
Sebastian <sebgertz@gmail.com>
2018-08-08 13:39:17 -0400

    root route drawn, signup and login forms added to users views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def welcome
+    @user = User.new
+    render 'welcome'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :users do
     resources :portfolios
-  end 
+  end
+
+  root to: 'application#welcome'
+  post   '/signin',   to: 'sessions#create'
+  get '/logout',  to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
