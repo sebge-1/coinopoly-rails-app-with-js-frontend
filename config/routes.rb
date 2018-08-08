9311bef93fe4c36415690059b127d50150a36def
Sebastian <sebgertz@gmail.com>
2018-08-07 23:24:07 -0400

    nest resource :portfolios under :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :users
+  resources :users do
+    resources :portfolios
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
