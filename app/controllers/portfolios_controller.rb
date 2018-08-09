60d5bad18481caae7ab522038904c0f66c525c80
Sebastian <sebgertz@gmail.com>
2018-08-09 14:21:26 -0400

    new portfolio workflow set up

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,17 @@
 class PortfoliosController < ApplicationController
+
+  def new
+    @user = current_user
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to portfolio_path(@portfolio)
+    else
+      redirect_to new_user_portfolio_path(current_user)
+    end
+  end
+  
 end
