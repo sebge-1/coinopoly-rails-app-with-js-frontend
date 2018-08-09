b3eee84992a9583a9d65f7ddab201c3c2fffd14f
Sebastian <sebgertz@gmail.com>
2018-08-09 08:25:03 -0400

    form partial for admins to submit new coins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include UsersHelper
 
   def create
     @user = User.new(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
     resources :portfolios
   end
 
+  resource :coins
+
   root to: 'application#welcome'
   post   '/signin',   to: 'sessions#create'
   get '/logout',  to: 'sessions#destroy'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
