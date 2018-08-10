b415585360c634cbf87f4792c83da6aa1f11d7c0
Sebastian <sebgertz@gmail.com>
2018-08-10 12:41:49 -0400

    added largest_position to user model, displayed on user#show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :portfolios
   has_many :positions, through: :portfolios
 
-  validates :name, presence: true 
+  validates :name, presence: true
   validates :email, presence: true
+
+  def largest_position
+    self.positions.max_by do |position|
+      position.value
+    end
+  end
 end
