a508632e0ddae4604ce62486d464287b8350846c
Sebastian <sebgertz@gmail.com>
2018-08-14 16:27:24 -0400

    delete admin column from users; cleaned up users show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,6 +9,6 @@ module UsersHelper
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :admin, :uid)
+    params.require(:user).permit(:name, :email, :password, :uid)
   end
 end
