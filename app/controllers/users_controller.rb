a261693f347e8b39537deddc15bfc5eb495ec182
Sebastian <sebgertz@gmail.com>
2018-08-16 18:48:27 -0400

    signup error message displays, follow/unfollow partials used to users/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:signup_error] = 'Something went wrong. Try again.'
       render :welcome
     end
   end
