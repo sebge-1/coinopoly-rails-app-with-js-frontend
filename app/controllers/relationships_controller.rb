dc846ee067c23dd1370de53a7c73657eb30a2639
Sebastian <sebgertz@gmail.com>
2018-08-16 14:31:44 -0400

    include follow activity in feed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
+    flash[:notice] = "You're now following #{user.name}"
     redirect_to users_path
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
+    flash[:notice] = "You've stopped following #{user.name}"
     redirect_to users_path
   end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,7 @@
 class Relationship < ApplicationRecord
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) {controller && controller.current_user}
+
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
