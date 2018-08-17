5b1788caeac2ed59448a3c252b15f1861b802987
Sebastian <sebgertz@gmail.com>
2018-08-17 17:46:37 -0400

    made _activity partial, refactored activities/index view and followers feed, set timezone to EST and fixed bug with helper method displayable

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,7 +6,7 @@ class ActivitiesController < ApplicationController
   end
 
   def follower_feed
-    @activities = self.index.select {|activity| current_user.follower_ids.include?(activity.owner_id)}
+      @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.follower_ids, owner_type: "User")
     render 'users/follower_feed'
   end
 
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,6 +1,6 @@
 module ActivitiesHelper
   def displayable?(activity)
-    activity.trackable_type == "Portfolio" && activity.trackable.public || activity.trackable_type == "Position" && activity.trackable.portfolio.public
+    true unless activity.trackable_type == "Portfolio" && !activity.trackable.public || activity.trackable_type == "Position" && !activity.trackable.portfolio.public
   end
 
 end
