71e1c1a4e33fd5bee6416304f398fe1bfc22ba83
Sebastian <sebgertz@gmail.com>
2018-08-18 16:17:10 -0400

    links added to navbar

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,6 +1,8 @@
 module ActivitiesHelper
   def displayable?(activity)
-    true unless activity.trackable_type == "Portfolio" && !activity.trackable.public || activity.trackable_type == "Position" && !activity.trackable.portfolio.public
+    if activity.trackable
+      true unless activity.trackable_type == "Portfolio" && !activity.trackable.public || activity.trackable_type == "Position" && !activity.trackable.portfolio.public
+    end
   end
 
 end
