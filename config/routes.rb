52ef572b358d51972be798a0be1eb89ad051f88b
Sebastian <sebgertz@gmail.com>
2018-08-23 09:04:53 -0400

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
 Rails.application.routes.draw do
-  get 'activities/index'
-  post "/admin/coin/new", to: 'coins#create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  root to: 'application#welcome'
+
+  get '/logout',  to: 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#create_from_facebook'
+  get 'users/:id/follower_feed' => 'activities#follower_feed'
+  get 'activities/index'
+
+  post   '/signin',   to: 'sessions#create'
+  post "/admin/coin/new", to: 'coins#create'
+
+  resources :coins
+  resources :relationships, only: [:create, :destroy]
+
   resources :users do
     resources :portfolios
     member do
       get :following, :followers
     end
   end
+  
   resources :portfolios do
     resources :positions
   end
-  resources :coins
-  resources :relationships, only: [:create, :destroy]
 
-  root to: 'application#welcome'
-  post   '/signin',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy'
-  get '/auth/facebook/callback' => 'sessions#create_from_facebook'
-  get 'users/:id/follower_feed' => 'activities#follower_feed'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
