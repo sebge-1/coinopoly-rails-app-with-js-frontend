79ca80e7de1c128f6fd15ac7b08adfd60901cb3b
Sebastian <sebgertz@gmail.com>
2019-01-08 00:15:27 -0500

    bug fix: fix more problems with nokogiri scraping and trailing slashes

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -22,13 +22,13 @@ class Coin < ApplicationRecord
 
   def set_img_url
     doc = Nokogiri::HTML(open("https://coinmarketcap.com/currencies/#{self.slugify_name}/", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    url = doc.css('body > div.container.main-section > div > div.col-lg-10.padding-top-1x > div.details-panel.flex-container.bottom-margin-2x > div.details-panel-item--header.flex-container > h1 > img').attr('src')
+    url = doc.css('body > div.container.main-section > div > div.col-xl-10.padding-top-1x > div.details-panel.flex-container.bottom-margin-2x > div.details-panel-item--header.flex-container > h1 > img').attr('src')
     self.update({img_url: url})
   end
 
   def set_ticker
-    doc = Nokogiri::HTML(open("https://coinmarketcap.com/currencies/#{self.slugify_name}", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-    ticker = doc.css('body > div.container.main-section > div > div.col-lg-10.padding-top-1x > div.details-panel.flex-container.bottom-margin-2x > div.details-panel-item--header.flex-container > h1 > span').inner_text
+    doc = Nokogiri::HTML(open("https://coinmarketcap.com/currencies/#{self.slugify_name}/", :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+    ticker = doc.css('body > div.container.main-section > div > div.col-xl-10.padding-top-1x > div.details-panel.flex-container.bottom-margin-2x > div.details-panel-item--header.flex-container > h1 > span').inner_text
     self.update({ticker: ticker.delete('()')})
   end
 
