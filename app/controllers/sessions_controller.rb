d7efd88a7a80489cb254cacf8a03f1abed72cc25
Sebastian <sebgertz@gmail.com>
2019-02-09 19:22:01 -0500

    run precompile on all assets for heroku deployment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,30 +15,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create_from_facebook
-    @called_omniauth = true
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.password = SecureRandom.urlsafe_base64 unless u.password != nil
-    end
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user.id)
-    else
-      flash[:error] = "Something went wrong. Try again."
-      @sign_up_user = User.new
-      @sign_in_user = User.new
-      render :welcome
-    end
-  end
-
   def destroy
     session.clear
     redirect_to root_path
   end
 
-  def auth
-    request.env['omniauth.auth']
-  end
 end
